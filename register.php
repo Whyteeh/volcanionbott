744f7f9aefb3345b1dd00cf1be44aff50258bc29
Whyteeh <35457852+Whyteeh@users.noreply.github.com>
2018-10-26 18:50:55 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://volcanionbot.herokuapp.com/execute.php';
+$BOT_TOKEN = '697732473:AAGtHI1q-IbeuYl3KiWN0UqV1pAJqvAWsmg';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
